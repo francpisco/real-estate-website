003b117b30f7583ad59ef96e3a824fb2967bebc4
Francisco Almeida <fmpapt@gmail.com>
2023-05-12 14:39:11 +0100

    Added contact form.

--- real_estate_website/main/routes.py
+++ real_estate_website/main/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request
+from real_estate_website.main.forms import ContactForm
 
 from real_estate_website.models import Article
 
@@ -15,3 +16,16 @@ def home():
 @main.route("/about")
 def about():
     return render_template("about.html", title="About")
+
+
+@main.route("/contact")
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        phone = form.phone.data
+        subject = form.subject.data
+        content = form.content.data
+
+    return render_template("contact.html", title="Contact us", form=form)
